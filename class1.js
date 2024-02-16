88d526bfae61781340d3c4121ec7365383b99df5
shuvo-99 <anontoshuvo99@gmail.com>
2024-02-16 21:46:04 +0600

    objects

--- class1.js
+++ class1.js
@@ -1,93 +1,93 @@
-// // Types - int, double, string, boolen, array, object
+// Types - int, double, string, boolen, array, object
 
-// // ====== Variables ========
+// ====== Variables ========
 
-// a = 10;
-// console.log(a);
+a = 10;
+console.log(a);
 
-// // Bad Practise - it can lead to unexpected behavior, especially in larger codebases where variable names might clash or be unintentionally overwritten.
+// Bad Practise - it can lead to unexpected behavior, especially in larger codebases where variable names might clash or be unintentionally overwritten.
 
-// // We use keyword - var, let, const
+// We use keyword - var, let, const
 
-// var b = 1;
-// let c = 1;
-// const d = 1;
+var b = 1;
+let c = 1;
+const d = 1;
 
-// // ======== Reassign ========
+// ======== Reassign ========
 
-// // Using var
-// var x = 10;
-// x = 20; // Reassigning the value of variable x
-// console.log(x); // Output: 20
+// Using var
+var x = 10;
+x = 20; // Reassigning the value of variable x
+console.log(x); // Output: 20
 
-// // Using let
-// let y = 30;
-// y = 40; // Reassigning the value of variable y
-// console.log(y); // Output: 40
+// Using let
+let y = 30;
+y = 40; // Reassigning the value of variable y
+console.log(y); // Output: 40
 
-// // Using const
-// const z = 50;
-// z = 60; // TypeError: Assignment to constant variable (cannot reassign const)
-// console.log(z);
+// Using const
+const z = 50;
+z = 60; // TypeError: Assignment to constant variable (cannot reassign const)
+console.log(z);
 
-// // ======= Redeclare ==========
+// ======= Redeclare ==========
 
-// // Using var
-// var x = 10;
-// var x = 20; // Redeclaration of variable x with var, no error
-// console.log(x); // Output: 20
+// Using var
+var x = 10;
+var x = 20; // Redeclaration of variable x with var, no error
+console.log(x); // Output: 20
 
-// // Using let
-// let y = 30;
-// let y = 40; // SyntaxError: Identifier 'y' has already been declared
+// Using let
+let y = 30;
+let y = 40; // SyntaxError: Identifier 'y' has already been declared
 
-// // Using const
-// const z = 50;
-// const z = 60; // SyntaxError: Identifier 'z' has already been declared
+// Using const
+const z = 50;
+const z = 60; // SyntaxError: Identifier 'z' has already been declared
 
-// // ======= Block Scope ==========
+// ======= Block Scope ==========
 
-// // Using var
-// var x = 10;
-// if (true) {
-//   var x = 15;
-//   console.log(x); // Output: 10
-// }
-// console.log(x); // Output: 10 (var is function-scoped)
+// Using var
+var x = 10;
+if (true) {
+  var x = 15;
+  console.log(x); // Output: 10
+}
+console.log(x); // Output: 10 (var is function-scoped)
 
-// // Using let
-// let y = 20;
-// if (true) {
-//   let y = 25;
-//   console.log(y); // Output: 20
-// }
-// console.log(y); // ReferenceError: y is not defined (y is block-scoped)
+// Using let
+let y = 20;
+if (true) {
+  let y = 25;
+  console.log(y); // Output: 20
+}
+console.log(y); // ReferenceError: y is not defined (y is block-scoped)
 
-// // Using const
-// const z = 30;
-// if (true) {
-//   const z = 30;
-//   console.log(z); // Output: 30
-//   // z = 40; // TypeError: Assignment to constant variable (cannot reassign const)
-// }
-// console.log(z); // ReferenceError: z is not defined (z is block-scoped)
+// Using const
+const z = 30;
+if (true) {
+  const z = 30;
+  console.log(z); // Output: 30
+  // z = 40; // TypeError: Assignment to constant variable (cannot reassign const)
+}
+console.log(z); // ReferenceError: z is not defined (z is block-scoped)
 
-// // ======= Hoisted ==========
+// ======= Hoisted ==========
 
-// // Using var
-// console.log(y); // ReferenceError: Cannot access 'y' before initialization
-// let y = 20;
-// console.log(y); // Output: 20
+// Using var
+console.log(y); // ReferenceError: Cannot access 'y' before initialization
+let y = 20;
+console.log(y); // Output: 20
 
-// // Using let
-// console.log(y); // ReferenceError: Cannot access 'y' before initialization
-// let y = 20;
-// console.log(y); // Output: 20
+// Using let
+console.log(y); // ReferenceError: Cannot access 'y' before initialization
+let y = 20;
+console.log(y); // Output: 20
 
-// // Using const
-// console.log(z); // ReferenceError: Cannot access 'z' before initialization
-// const z = 30;
-// console.log(z); // Output: 30
+// Using const
+console.log(z); // ReferenceError: Cannot access 'z' before initialization
+const z = 30;
+console.log(z); // Output: 30
 
 // =============================
 // =========== ARRAY ===========
@@ -183,3 +183,50 @@ console.log(newSpreadArr);
 // Array Destructing
 let [first, second, third] = arr;
 console.log(second);
+
+// =============================
+// =========== OBJECT ===========
+// =============================
+
+const student = {
+  // { property : value}
+  name: "Ghost",
+  age: 20,
+  semester: 3,
+};
+
+console.log(student);
+
+// Spread Operator
+const updatedStudent = { ...student, home: "Dhaka" };
+console.log(updatedStudent);
+
+const updatedStudentSpread = { ...student };
+
+const std = student;
+student.rs = true;
+console.log(std);
+console.log(updatedStudentSpread);
+
+// Object Destructuring
+const { age } = student;
+console.log(age);
+
+// Access properties
+console.log(student.age);
+console.log(student[age]);
+
+// Delete
+delete student.age;
+// or
+delete student["age"];
+
+console.log(student);
+
+// Template literals (Template strings)
+console.log(`string text line 1
+ string text line 2`);
+
+let names = "Ghost";
+
+console.log(`Hi i am ${names}`);
