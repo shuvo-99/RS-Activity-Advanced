181914977054ce42b21b4a3f0afda34b28da9d61
shuvo-99 <anontoshuvo99@gmail.com>
2024-02-16 15:05:13 +0600

    initialize

---
+++ class1.js
@@ -0,0 +1,8 @@
+// Types - int, double, string, boolen, array, object
+
+// ====== Variables ========
+
+x = 10;
+console.log(x);
+
+// Bad Practise - it can lead to unexpected behavior, especially in larger codebases where variable names might clash or be unintentionally overwritten.
